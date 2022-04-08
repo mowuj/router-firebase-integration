9d8750618a6b3de3726c511426b5be5257e09a27
mowuj <mowuj92@gmail.com>
2022-04-08 23:51:24 +0600

    signin with google

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const { signInWithGoogle } = useFirebase();
     return (
         <div>
             <h2>Please Login</h2>
+            <form action="">
+                
+                <input type="email" name="" id="" placeholder='Your Email' />
+                <br />
+                <input type="password" name="" id="" placeholder='Enter Your Password' />
+                <br />
+                <input type="submit" value="Login" />
+            </form>
+            <div style={{margin:'20px'}}>
+                <button onClick={signInWithGoogle}>Google Sign In</button>
+            </div>
         </div>
     );
 };
---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,19 @@
+import { useState } from "react"
+import { GoogleAuthProvider,getAuth, signInWithPopup } from "firebase/auth";
+import app from '../Firebase/firebase.init'
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+const useFirebase = () => {
+    const [user, setUser] = useState({})
+    
+    const signInWithGoogle = () => {
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                const user = result.user;
+                setUser(user)
+                console.log(user)
+        })
+    }
+    return {user,signInWithGoogle}
+}
+export default useFirebase;
